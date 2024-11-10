4306f54779ced362ff9371609f98b15d6ab8c7fa
Manas M Salian <147305419+Manas-salian@users.noreply.github.com>
2024-11-10 19:16:02 +0530

    changes

--- src/components/Cake.jsx
+++ src/components/Cake.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Cake = ({ cake, onAddToCart }) => {
     return (
         <div className="cake">
-            <img src={`/assets/${cake.image}`} alt={cake.name} /> {/* Note the path */}
+            <img src={cake.image} alt={cake.name} /> {/* Note the path */}
             <h3>{cake.name}</h3>
             <p>Price: ${cake.price}</p>
             <button onClick={() => onAddToCart(cake)}>Add to Cart</button>
